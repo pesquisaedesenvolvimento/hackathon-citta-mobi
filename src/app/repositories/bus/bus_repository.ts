e17e2a8d5f4816748d180676610fc917625de18c
FelipeQueFez <felipequefez@gmail.com>
2020-06-23 23:51:49 -0300

    refactor url

--- src/app/repositories/bus/bus_repository.ts
+++ src/app/repositories/bus/bus_repository.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Bus } from './models/bus';
 import { Observable } from 'rxjs';
+import { _baseUrl } from '../environment';
 
 @Injectable()
 export class BusRepository {
     constructor(private http: HttpClient) { }
 
     public getAll(): Observable<Bus[]> {
-        return this.http.get<Bus[]>('https://138fk4ea41.execute-api.eu-west-1.amazonaws.com/dev/bus');
+        return this.http.get<Bus[]>(`${_baseUrl}bus`);
     }
 }
